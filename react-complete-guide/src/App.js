beacc03854e8e636006f4f3d96bbc7443d207bf2
Tushar Gupta <gupta.tus@husky.neu.edu>
2018-07-07 23:18:02 -0400

    learning app

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,12 +1,48 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
+
+  state = {
+    person: [
+      {name:'Max' , age: 28 },
+      {name:'Samnu' , age: 26 }
+    ]
+  }
+
+  SateHandler = (newValue) => {
+    console.log("was clicked !")
+
+    this.setState({
+      person: [
+        {name:newValue , age: 28 },
+        {name:'Samnu' , age: 30 }
+      ]
+    })
+    
+  }
+
+  nameChangedHandler = (event) => {
+    
+    this.setState({
+      person: [
+        {name:"Tushar" , age: 28 },
+        {name:event.target.value , age: 30 }
+      ]
+    })
+  }
   render() {
     return (
       <div className="App">
        <h1>Hi I am a React App</h1>
+       <button onClick={this.SateHandler.bind(this,"Sanjeev")}>Switch</button>
+       <Person click={this.SateHandler.bind(this,"Suman")} 
+       name={this.state.person[0].name}
+       age={this.state.person[0].age}> my hobbies are XYZ</Person>
+       <Person name={this.state.person[1].name} age={this.state.person[1].age} change={this.nameChangedHandler}/>
+       <Person />
       </div>
     );
   }
---
+++ react-complete-guide/src/Person/Person.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'; 
+import './Person.css';
+
+const person = (props) => {
+
+    return (
+        <div className="Person">
+            <p onClick={props.click}> my name is {props.name} and my age is {props.age}</p>
+            <p> {props.children} </p>
+            <input onChange={props.change} value={props.name}/>
+        </div>
+    
+    )
+}
+
+export default person;
